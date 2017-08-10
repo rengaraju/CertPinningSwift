fa42d0eeb4ff6a7a555b7d9049519a798ed12e6b
Shahrukh Shikalgar <shahrukh@ascratech.com>
2017-08-10 13:52:02 +0530

    initial commit

--- CertPinning/ViewController.swift
+++ CertPinning/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		callGetRequest()
+		callCertificatePinning()
+		
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 
@@ -20,6 +25,34 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	func callCertificatePinning() {
+		let serverTrustPolicies: [String: ServerTrustPolicy]  = [
+			"github.com": .pinCertificates(
+				certificates: ServerTrustPolicy.certificates(),
+				validateCertificateChain: true,
+				validateHost: true
+			)
+		]
+		
+		let sessionManager = SessionManager(serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies))
+		
+		sessionManager.request("https://github.com").response { response in // method defaults to `.get`
+			debugPrint(response)
+		}
+		
+	}
+	
+	func callGetRequest() {
+		Alamofire.request("https://github.com").response { response in
+			print("Request:", response.request ?? String())
+			print("\n\nResponse:",response.response ?? String())
 
+			
+			if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+				print("Data: \(utf8Text)")
+			}
+		}
+	}
+	
 }
 
